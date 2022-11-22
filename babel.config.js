f0f5706129b7f7a54e9143b0de32293df37aa3fa
Ezequiel <ezequielperezpc@gmail.com>
2022-11-22 00:29:58 -0300

    Movak - first commit

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '@': './src',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@api': './src/api',
+          '@navigation': './src/navigation',
+        },
+        extensions: ['.ios.ts', '.android.ts', '.ts', '.ios.tsx', '.android.tsx', '.tsx', '.jsx', '.js', '.json'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
+}
